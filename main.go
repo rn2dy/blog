0bc6a23152f45be7f82ad75c5ad3ac38b3374854
rn2dy <baboonworksfine@hotmail.com>
2022-03-01 14:02:46 -0800

    wip

--- main.go
+++ main.go
@@ -2,9 +2,16 @@
 
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
+	envport := os.Getenv("PORT")
 	mux.logger = &SimpleLogger{}
+	if envport != "" {
+		port = envport
+	}
 	http.ListenAndServe(port, mux)
 }
